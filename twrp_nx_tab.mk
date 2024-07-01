f7e11599bab0355a3ceb2718c257286b8e6736db
sans30000n-gh <sans30000n-gh@Outlook.com>
2024-07-01 18:19:34 +0700

    Update twrp_nx_tab.mk

--- twrp_nx_tab.mk
+++ twrp_nx_tab.mk
@@ -7,10 +7,15 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit full configuration for nx.
+include device/nintendo/nx/lineage.mk    
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration for nx.
+$(call inherit-product, device/nintendo/nx/device.mk)
 
 # Inherit from nx_tab device
 $(call inherit-product, device/nintendo/nx_tab/device.mk)
@@ -20,10 +25,3 @@ PRODUCT_NAME := twrp_nx_tab
 PRODUCT_BRAND := NINTENDO
 PRODUCT_MODEL := Switch
 PRODUCT_MANUFACTURER := nintendo
-
-PRODUCT_GMS_CLIENTID_BASE := android-nvidia
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lineage_nx_tab-userdebug 14 AP1A.240505.005 4015571928 release-keys"
-
-BUILD_FINGERPRINT := NVIDIA/nx/nx:11/RQ1A.210105.003/7825230_3167.5736:user/release-keys
